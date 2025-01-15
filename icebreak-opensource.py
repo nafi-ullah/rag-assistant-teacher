a3f9f7dffddbd70c562f6b2d9c8a5ff5d1bfe301
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2025-01-15 06:44:54 +0600

    change models

--- icebreak-opensource.py
+++ icebreak-opensource.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
     # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model_name="llama3")
+    llm = ChatOllama(model_name="llama3") # llm = ChatOllama(model_name="mistral")
 
     chain = summary_prompt_template | llm
 
