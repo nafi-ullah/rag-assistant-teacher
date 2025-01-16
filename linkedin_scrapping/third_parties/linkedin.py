4216c379957bb39a351eb0d3216327ce0c06ddfc
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2025-01-16 13:19:04 +0600

    addded linkedin scrapping code

---
+++ linkedin_scrapping/third_parties/linkedin.py
@@ -0,0 +1,56 @@
+# The mock equals false is a boolean that we want to put for mocking and for local development.
+# So in the docstring you can see scrape information from LinkedIn profile.
+
+# Now I want to show you that our mock is working.
+# So I'm simply going to add the mock equals true, which is going to take the data from the gist file
+# mane mock hoile mockserver mane githubgist theke nibe otherwise linekdin parse kore nibe cz ekhane api credit limited.
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+        response = requests.get(
+            api_endpoint,
+            params={"url": linkedin_profile_url},
+            headers=header_dic,
+            timeout=10,
+        )
+
+
+    # for token limit problem.. jegula empty file ogula jeno remove mare..
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/",
+        )
+    )
---
+++ runmain.py
@@ -0,0 +1,4 @@
+from linkedin_scrapping.ice_breaker import main_process
+
+if __name__ == "__main__":
+    main_process()
