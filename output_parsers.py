7d3b780cb384a2f583008b15f507d90243c8a17d
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2025-01-19 13:23:02 +0600

    added output parser code

--- output_parsers.py
+++ output_parsers.py
@@ -1,6 +1,9 @@
 from typing import List, Dict, Any
 
 from langchain_core.output_parsers import PydanticOutputParser
+# So we want to import list and any. So we want to use for typing. And we want to use the pedantic output parser. So what's pedantic in a nutshell?
+# It's very similar to the built in data class in Python, but it's actually an external library that is helping us with data validation and setting management. So it will allow us to define schemas and to validate inputs against those schemas.
+
 from pydantic import BaseModel, Field
 
 
